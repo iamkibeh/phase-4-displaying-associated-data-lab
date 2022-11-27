48ed8b1571bdd7be4e873ad49a39de5f7ce50047
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-11-27 10:03:58 +0300

    complete assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+    def show
+        user = User.find_by!(id: params[:id])
+        render json: user, include: :items
+    end
+
+    def record_not_found
+        render json: {error: "record_not_found"}, status: :not_found
+    end
 end
